0c842160a51a24f039ad02c3760bd6ef07b6d18c
kapolandia <paykapo4@gmail.com>
2024-05-21 17:46:51 +0200

    push movies

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+
+    public function home(){
+        $movies = Movie::all();
+
+        $data = [
+            'movies' => $movies
+        ];
+
+        return view('home', $data);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.app')
 
 @section('main')
-    <div class="container">
-        ciao
+    <div class="container row mx-auto mt-3 m-5 justify-content-center">
+        @foreach ($movies as $movie)
+        <div class="card col-3 m-4 p-0" >
+            <img src="{{Vite::asset("resources/images/logo.png")}}" class="card-img-top">
+            <div class="card-header text-white bg-primary" style="height: 60px">
+                {{ $movie->title }}
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">{{ $movie->original_title }}</li>
+                <li class="list-group-item">{{ $movie->nationality }}</li>
+                <li class="list-group-item">{{ $movie->date }}</li>
+                <li class="list-group-item">{{ $movie->vote }}</li>
+            </ul>
+        </div>
+        @endforeach
     </div>
 @endsection
